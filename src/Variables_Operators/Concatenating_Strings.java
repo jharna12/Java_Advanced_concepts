5ffa6a610459d88786df656ccecf093a8d4eec8e
jharna agrawal <jharna.agrawal_cs16@gla.ac.in>
2020-10-22 15:52:45 +0530

    concatenating Strings

---
+++ src/Variables_Operators/Concatenating_Strings.java
@@ -0,0 +1,13 @@
+package Variables_Operators;
+
+public class Concatenating_Strings {
+	public static void main(String[] args) {
+		String first="Concatenated ";
+		String second= "String";
+		String outcome=first+second;
+		System.out.println("final outcome: " +outcome +"!");
+		int number= 5;
+		System.out.println("Concatenated String with integer " + outcome +" "+number+"!");
+	}
+
+}
